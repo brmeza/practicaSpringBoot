7e01a92eac8e93c4fd6f96335c8ca269d801456a
brmeza <brayanmesz1998@gmail.com>
2023-07-12 10:52:47 -0500

    anexos a la carpeta static

---
+++ src/main/java/com/cursospring/curso/controllers/UsuarioController.java
@@ -0,0 +1,12 @@
+package com.cursospring.curso.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UsuarioController {
+    @RequestMapping(value = "prueba")
+    public  String prueba(){
+        return "prueba";
+    }
+}
